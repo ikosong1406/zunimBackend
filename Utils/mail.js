d95b2e103301e017f657402a2083f6670938e7ac
ikosong14 <alexandervirtuous14@gmail.com>
2024-08-02 15:48:28 +0100

    modified:   Utils/mail.js

--- Utils/mail.js
+++ Utils/mail.js
@@ -1,26 +1,17 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "mail.bitvelar.com",
+  host: "mail.zunim.com.ng",
   port: 465, //465 is true & other port are false
   secure: true,
   auth: {
-    user: "support@bitvelar.com",
-    pass: "Bitvelar@14",
+    user: "support@zunim.com.ng",
+    pass: "Zunimsupport@14",
   },
   tls: {
     rejectUnauthorized: false,
   },
 });
-
-// var transporter = nodemailer.createTransport({
-//   host: "sandbox.smtp.mailtrap.io",
-//   port: 2525,
-//   auth: {
-//     user: "b8a2ba1a05b14a",
-//     pass: "48b61489156df6",
-//   },
-// });
 
 async function sendMail(to, subject, text, html) {
   const mailOptions = {
