7be854fcc04bdd0fc7817d2341751e9d74d6587e
ikosong14 <alexandervirtuous14@gmail.com>
2024-08-02 18:55:16 +0100

    modified:   Routes/createOrders.js

--- Routes/createOrders.js
+++ Routes/createOrders.js
@@ -7,19 +7,19 @@ const twilio = require("twilio");
 require("dotenv").config(); // Load environment variables
 
 // Twilio configuration using environment variables
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = twilio(accountSid, authToken);
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const client = twilio(accountSid, authToken);
 
 // Helper function to format Nigerian phone numbers
-const formatPhoneNumber = (phoneNumber) => {
-  // Remove any non-digit characters
-  const cleanedNumber = phoneNumber.replace(/\D/g, "");
-  // Replace leading 0 with +234
-  return cleanedNumber.startsWith("0")
-    ? `+234${cleanedNumber.substring(1)}`
-    : `+234${cleanedNumber}`;
-};
+// const formatPhoneNumber = (phoneNumber) => {
+
+//   const cleanedNumber = phoneNumber.replace(/\D/g, "");
+
+//   return cleanedNumber.startsWith("0")
+//     ? `+234${cleanedNumber.substring(1)}`
+//     : `+234${cleanedNumber}`;
+// };
 
 router.post("/", async (req, res) => {
   const {
@@ -71,14 +71,13 @@ router.post("/", async (req, res) => {
     // Send order confirmation email
     sendMail(newOrder.customer.email, "Order Confirmation", "", htmlContent);
 
-    // Format phone number and send SMS
-    const formattedPhoneNumber = formatPhoneNumber(phoneNumber);
-    const smsBody = `Hello ${firstName}, your order with Zunim has been confirmed. Thank you for shopping with us!`;
-    await client.messages.create({
-      body: smsBody,
-      from: process.env.TWILIO_PHONE_NUMBER, // Your Twilio number from environment variable
-      to: formattedPhoneNumber, // Customer's phone number
-    });
+    // const formattedPhoneNumber = formatPhoneNumber(phoneNumber);
+    // const smsBody = `Hello ${firstName}, your order with Zunim has been confirmed. Thank you for shopping with us!`;
+    // await client.messages.create({
+    //   body: smsBody,
+    //   from: process.env.TWILIO_PHONE_NUMBER,
+    //   to: formattedPhoneNumber,
+    // });
 
     return res.status(200).json({
       status: "ok",
